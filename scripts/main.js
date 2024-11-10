209f518fee42f737dd2c42faa972ab146ef4715f
talesCPV <tales@flexibus.com.br>
2024-11-10 20:28:24 -0300

    by_script

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,19 @@ document.querySelector('#btn-viewCob').addEventListener('click',()=>{
 document.querySelector('#btn-novaCob').addEventListener('click',()=>{
     openHTML('novaCobranca')
 })
+
+document.querySelector('#btn-webhook').addEventListener('click',()=>{
+    
+    const data = new URLSearchParams()
+        data.append("body", '{"teste1" : "valor_1","number":2}')
+        data.append("cust", 5)
+
+    const myRequest = new Request("asaas/webhook.php",{
+        method : "POST",
+        body : data
+    })    
+    
+    return fetch(myRequest)
+
+
+})
